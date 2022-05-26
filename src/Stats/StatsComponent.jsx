91e5b10f1a2e3d2b886dec91fa5fc133bfaf52a5
Abdel Raoof <olakara@gmail.com>
2022-05-26 06:20:04 +0400

    local: code update

--- src/Stats/StatsComponent.jsx
+++ src/Stats/StatsComponent.jsx
@@ -4,16 +4,21 @@ import StatsPresenter from "./StatsPresenter";
 export default function StatsComponent() {
   let statsPresenter = new StatsPresenter();
   const [stateViewModel, copyViewModelToStateViewModel] = useState("");
+  const [counterViewModel, setBookCountViewModel] = useState("");
 
   useEffect(() => {
     statsPresenter.load((generatedViewModel) => {
       copyViewModelToStateViewModel(generatedViewModel);
     });
+
+    statsPresenter.getBookCount((generatedViewModel) => {
+      setBookCountViewModel(generatedViewModel);
+    });
   }, []);
 
   return (
     <div>
-      <h3>Book Count </h3>
+      <h5>Book Count {counterViewModel}</h5>
       <h5>Last Added Book (ui)</h5>
       {stateViewModel}
     </div>
